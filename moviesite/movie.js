a0459c067cdb81fe39c3b9188eb08915becfef93
Erekle99 <erekledateshidze99@gmail.com>
2022-07-15 00:32:10 +0400

    7
    7

--- moviesite/movie.js
+++ moviesite/movie.js
@@ -29,7 +29,7 @@ function showMovies(movies) {
         <h3>${title}</h3>
         <span class = "${getClassByRate(vote_average)}"> ${vote_average}</span>
         </div>
-        <div class="oveview">
+        <div class="overview">
         <h3>Overview</h3>
         ${overview}
         </div>
